a79b41f7e96bed1f58582ee23a57e4c22dd1a38c
Fizan Muhammed Faisal <145630603+FizanMuhammedFaisal@users.noreply.github.com>
2026-03-08 12:39:43 +0530

    feat(operator): operator service intialized

---
+++ apps/operator/src/main.ts
@@ -0,0 +1,9 @@
+import { Elysia } from 'elysia';
+
+const app = new Elysia();
+
+app.get('/', () => 'Hello World');
+
+app.listen(3000);
+
+console.log(`🦊 Elysia is running at http://${app.server?.hostname}:${app.server?.port}`);
