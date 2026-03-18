5302d7c3395bd7c702fa46cadb4ae771deb62805
Fizan Muhammed Faisal <145630603+FizanMuhammedFaisal@users.noreply.github.com>
2026-03-18 10:22:35 +0530

    feat(grpc-lib): add both services and first rpc call proto

--- apps/operator/src/main.ts
+++ apps/operator/src/main.ts
@@ -4,13 +4,13 @@ import type { CRDManager } from './infrastructure/k8s/ensureCrd';
 import type { GRpcServer } from './presentation/grpc/rpc-server';
 import { TOKENS } from './config/di/tokens';
 async function main() {
-    const rpcServer = container.resolve<GRpcServer>(TOKENS.GRPCServer);
+    const grpcServer = container.resolve<GRpcServer>(TOKENS.GRPCServer);
     const crdManager = container.resolve<CRDManager>(TOKENS.CRDManager);
     //ensure the torq crd is preset
     // ensureCrd()
     await crdManager.ensureCrd();
     //  start grpc server
-    await rpcServer.start();
+    await grpcServer.start();
 
     // connect to message queue
 
